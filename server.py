6b401b4df9c0f5c1954f60191bf910cdf9401470
Brian Warehime <brianwarehime@gmail.com>
2015-09-08 11:22:58 -0400

    Beta version 2.0 - complete UI redesign

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ########################################################
-# threat_note v1.0                                     #
+# threat_note v2.0                                     #
 # Developed By: Brian Warehime                         #
 # Defense Point Security (defpoint.com)                #
 # August 24, 2015                                      #
@@ -11,7 +11,7 @@
 # Imports #
 ###########
 from flask.ext.pymongo import PyMongo
-from flask import Flask, jsonify, make_response, render_template, request
+from flask import Flask, jsonify, make_response, render_template, request, url_for, redirect
 from werkzeug.datastructures import ImmutableMultiDict
 import json
 import bson
@@ -23,6 +23,7 @@ import whois
 import re
 import ast
 from bson.son import SON
+import csv
 
 #################
 # Configuration #
@@ -68,9 +69,9 @@ def home():
             typedict["category"] = i
             tempx = x / counts
             newtemp = tempx * 100
-            typedict["value"] = round(newtemp,2)
+            typedict["value"] = x
             typelist.append(typedict.copy())
-        return render_template('home.html', networks=dictlist, network=network, favs=favs, typelist=typelist)
+        return render_template('dashboard.html', networks=dictlist, network=network, favs=favs, typelist=typelist)
     except Exception as e:
         return render_template('error.html', error=e)
 
@@ -119,21 +120,10 @@ def campaignsummary(uid):
         whoisdata=""
         settingsvars = mongo.db.settings.find()
         # Run ipwhois or domainwhois based on the type of indicator
-        if str(http['inputtype']) == "IPv4" or str(http['inputtype']) == "IPv6":
-            jsonvt = vt_ipv4_lookup(str(http['object']))
-            whoisdata = ipwhois(str(http['object']))
-        elif str(http['inputtype']) == "Domain":
-            whoisdata = domainwhois(str(http['object']))
-            jsonvt = vt_domain_lookup(str(http['object']))
-        return render_template('object.html', records=http, jsonvt=jsonvt, whoisdata=whoisdata,settingsvars=settingsvars)
-    except Exception as e:
-        return render_template('error.html', error=e)
-
-@app.route('/indicators', methods=['GET'])
-def indicators():
-    try:
-        indicators = mongo.db.network.find()
-        return render_template('indicators.html', network=indicators)
+        if str(http['inputtype']) == "IPv4" or str(http['inputtype']) == "IPv6" or str(http['inputtype']) == "Domain" or str(http['inputtype']) == "Network":
+            return redirect(url_for('objectsummary', uid=http['_id']))
+        else:
+           return redirect(url_for('threatactorobject', uid=http['_id']))
     except Exception as e:
         return render_template('error.html', error=e)
 
@@ -147,57 +137,68 @@ def newobj():
 @app.route('/insert/object/', methods=['POST'])
 def newobject():
     try:
-        inputobject = request.form.get('inputobject')
-        inputuid = request.form.get('inputuid')
-        inputfirstseen = request.form.get('inputfirstseen')
-        inputlastseen = request.form.get('inputlastseen')
-        inputcampaign = request.form.get('inputcampaign')
-        inputcomments = request.form.get('inputcomments')
-        inputtype = request.form.get('inputtype')
-        diamondmodel = request.form.get('diamondmodel')
+        something = request.form
+        imd = ImmutableMultiDict(something)
+        records = convert(imd)
+        newdict = {}
+        for i in records:
+            newdict[i] = records[i]
         # Makes sure if you submit an IPv4 indicator, it's an actual IP address.
-        ipregex = re.match( r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',inputobject)
-        if inputtype == "IPv4":
+        ipregex = re.match( r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',newdict['inputobject'])
+        if newdict['inputtype'] == "IPv4":
             if ipregex:     
-                if mongo.db.network.find({"object":inputobject}).count() > 0:
+                if mongo.db.network.find({"object":newdict['inputobject']}).count() > 0:
                     errormessage = "Entry already exists in database."
-                    return render_template('newobject.html', errormessage=errormessage, inputuid=inputuid, inputtype=inputtype, inputobject=inputobject, inputfirstseen=inputfirstseen, inputlastseen=inputlastseen, inputcampaign=inputcampaign, inputcomments=inputcomments, diamondmodel=diamondmodel)
+                    return render_template('newobject.html', errormessage=errormessage, inputtype=newdict['type'], inputobject=newdict['inputobject'], inputfirstseen=newdict['inputfirstseen'], inputlastseen=newdict['inputlastseen'], inputcampaign=newdict['inputcampaign'], inputcomments=newdict['inputcomments'], diamondmodel=newdict['diamondmodel'])
                 else:
-                    inputobject = inputobject.strip()
-                    newdata = {"object":inputobject, "firstseen":inputfirstseen,"lastseen":inputlastseen,"campaign":inputcampaign,"comments":inputcomments,"inputtype":inputtype, "diamondmodel":diamondmodel, "favorite":"False"}
+                    inputobject = newdict['inputobject'].strip()
+                    newdata = {"object":newdict['inputobject'], "firstseen":newdict['inputfirstseen'],"lastseen":newdict['inputlastseen'],"campaign":newdict['inputcampaign'],"comments":newdict['inputcomments'],"inputtype":newdict['inputtype'], "diamondmodel":newdict['diamondmodel'], "favorite":"False"}
                     mongo.db.network.insert(newdata)
-                    network = mongo.db.network.find()
+                    network = mongo.db.network.find({"$or": [{"inputtype": "IPv4"}, {"inputtype":"Network"},{"inputtype": "IPv6"}, {"inputtype": "Domain"}]})
+                    return render_template('networks.html', network=network)
             else:
                 errormessage = "Not a valid IP Address."
-                return render_template('newobject.html', errormessage=errormessage, inputuid=inputuid, inputtype=inputtype, inputobject=inputobject, inputfirstseen=inputfirstseen, inputlastseen=inputlastseen, inputcampaign=inputcampaign, inputcomments=inputcomments, diamondmodel=diamondmodel)
+                return render_template('newobject.html', errormessage=errormessage, inputtype=newdict['inputtype'], inputobject=newdict['inputobject'], inputfirstseen=newdict['inputfirstseen'], inputlastseen=newdict['inputlastseen'], inputcampaign=newdict['inputcampaign'], inputcomments=newdict['inputcomments'], diamondmodel=newdict['diamondmodel'])
         else:
-            if mongo.db.network.find({"object":inputobject}).count() > 0:
+            if mongo.db.network.find({"object":newdict['inputobject']}).count() > 0:
                     errormessage = "Entry already exists in database."
-                    return render_template('newobject.html', errormessage=errormessage, inputuid=inputuid, inputtype=inputtype, inputobject=inputobject, inputfirstseen=inputfirstseen, inputlastseen=inputlastseen, inputcampaign=inputcampaign, inputcomments=inputcomments, diamondmodel=diamondmodel)
+                    return render_template('newobject.html', errormessage=errormessage, inputtype=newdict['inputtype'], inputobject=newdict['inputobject'], inputfirstseen=newdict['inputfirstseen'], inputlastseen=newdict['inputlastseen'], inputcampaign=newdict['inputcampaign'], inputcomments=newdict['inputcomments'], diamondmodel=newdict['diamondmodel'])
             else:
-                inputobject = inputobject.strip()
-                newdata = {"object":inputobject, "firstseen":inputfirstseen,"lastseen":inputlastseen,"campaign":inputcampaign,"comments":inputcomments,"inputtype":inputtype, "diamondmodel":diamondmodel, "favorite": "False"}
+                inputobject = newdict['inputobject'].strip()
+                newdata = {"object":newdict['inputobject'], "firstseen":newdict['inputfirstseen'],"lastseen":newdict['inputlastseen'],"campaign":newdict['inputcampaign'],"comments":newdict['inputcomments'],"inputtype":newdict['inputtype'], "diamondmodel":newdict['diamondmodel'], "favorite":"False"}
                 mongo.db.network.insert(newdata)
-                network = mongo.db.network.find()
+                if newdata['inputtype'] == "IPv6" or newdata['inputtype'] == "Domain" or newdata['inputtype'] == "Network":
+                    network = mongo.db.network.find({"$or": [{"inputtype": "IPv4"}, {"inputtype":"Network"},{"inputtype": "IPv6"}, {"inputtype": "Domain"}]})
+                    return render_template('networks.html', network=network)
+                else:
+                    threatactors = mongo.db.network.find({"inputtype":"Threat Actor"})
+                    return render_template('threatactors.html', network=threatactors)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+@app.route('/edit/<uid>', methods=['POST', 'GET'])
+def editobject(uid):
+    try:
+        http = mongo.db.network.find_one({'_id':bson.ObjectId(oid=str(uid))})
+        return render_template('neweditobject.html', entry=http)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+@app.route('/delete/network/<uid>', methods=['GET'])
+def deletenetworkobject(uid):
+    try:
+        mongo.db.network.remove({'_id':bson.ObjectId(oid=str(uid))})
+        network = mongo.db.network.find({"$or": [{"inputtype": "IPv4"}, {"inputtype":"Network"},{"inputtype": "IPv6"}, {"inputtype": "Domain"}]})
         return render_template('networks.html', network=network)
     except Exception as e:
         return render_template('error.html', error=e)
 
-@app.route('/edit/object/', methods=['POST'])
-def editobject():
-    try:
-        records = request.form.get('records')
-        entry = list(mongo.db.network.find({'_id':bson.ObjectId(oid=str(records))}))
-        return render_template('editobject.html', entry=entry)
-    except Exception as e:
-        return render_template('error.html', error=e)
-
-@app.route('/delete/<uid>/', methods=['GET'])
-def deleteobject(uid):
+@app.route('/delete/threatactor/<uid>', methods=['GET'])
+def deletethreatactorobject(uid):
     try:
         mongo.db.network.remove({'_id':bson.ObjectId(oid=str(uid))})
-        network = mongo.db.network.find()
-        return render_template('indicators.html', network=network)
+        threatactors = mongo.db.network.find({"inputtype":"Threat Actor"})
+        return render_template('threatactors.html', network=threatactors)
     except Exception as e:
         return render_template('error.html', error=e)
 
@@ -223,7 +224,7 @@ def updatesettings():
             mongo.db.settings.update( { '_id': { '$exists': True } }, {'$set': {'apikey':newdict['apikey']}})
         else:
             mongo.db.settings.insert(newdict)
-        newrecords = mongo.db.settings.find()
+        newrecords = mongo.db.settings.find_one()
         return render_template('settings.html', records=newrecords)
     except Exception as e:
         return render_template('error.html', error=e)
@@ -246,14 +247,21 @@ def updateobject():
         http = mongo.db.network.find_one({'_id':bson.ObjectId(oid=str(records['_id']))})
         jsonvt=""
         whoisdata=""
-        settingsvars = mongo.db.settings.find()
-        if str(http['inputtype']) == "IPv4" or str(http['inputtype']) == "IPv6":
-            jsonvt = vt_ipv4_lookup(str(http['object']))
-            whoisdata = ipwhois(str(http['object']))
-        elif str(http['inputtype']) == "Domain":
-            whoisdata = domainwhois(str(http['object']))
-            jsonvt = vt_domain_lookup(str(http['object']))
-        return render_template('object.html', records=http, jsonvt=jsonvt, whoisdata=whoisdata,settingsvars=settingsvars, something=something)
+        settingsvars = mongo.db.settings.find_one()
+        if newdict['inputtype'] == "IPv4" or newdict['inputtype'] == "IPv6":
+            if settingsvars['whoisinfo'] == "on":
+                whoisdata = ipwhois(str(http['object']))
+            if settingsvars['vtinfo'] == "on":
+                jsonvt = vt_ipv4_lookup(str(http['object']))
+        elif newdict['inputtype'] == "Domain":
+            if settingsvars['whoisinfo'] == "on":
+                whoisdata = ipwhois(str(http['object']))
+            if settingsvars['vtinfo'] == "on":
+                jsonvt = vt_ipv4_lookup(str(http['object']))
+        if newdict['inputtype'] == "Threat Actor":
+            return render_template('threatactorobject.html', records=http, jsonvt=jsonvt, whoisdata=whoisdata,settingsvars=settingsvars)
+        else:
+            return render_template('networkobject.html', records=http, jsonvt=jsonvt, whoisdata=whoisdata,settingsvars=settingsvars)
     except Exception as e:
         return render_template('error.html', error=e)
 
@@ -264,7 +272,7 @@ def insertnewfield():
         imd = ImmutableMultiDict(something)
         records = convert(imd)
         newdict = {}
-        dictlist = []
+        #dictlist = []
         for i in records:
             if i == "inputnewfieldname":
                 newdict[records[i]] = records['inputnewfieldvalue']
@@ -272,8 +280,8 @@ def insertnewfield():
                 pass
             else:
                 newdict[i] = records[i]
-        dictlist.append(newdict)
-        return render_template('editobject.html', entry=dictlist)
+        #dictlist.append(newdict)
+        return render_template('neweditobject.html', entry=newdict)
     except Exception as e:
         return render_template('error.html', error=e)
 
@@ -283,7 +291,7 @@ def objectsummary(uid):
         http = mongo.db.network.find_one({'_id':bson.ObjectId(oid=str(uid))})
         jsonvt=""
         whoisdata=""
-        settingsvars = mongo.db.settings.find()
+        settingsvars = mongo.db.settings.find_one()
         # Run ipwhois or domainwhois based on the type of indicator
         if str(http['inputtype']) == "IPv4" or str(http['inputtype']) == "IPv6":
             jsonvt = vt_ipv4_lookup(str(http['object']))
@@ -291,7 +299,22 @@ def objectsummary(uid):
         elif str(http['inputtype']) == "Domain":
             whoisdata = domainwhois(str(http['object']))
             jsonvt = vt_domain_lookup(str(http['object']))
-        return render_template('object.html', records=http, jsonvt=jsonvt, whoisdata=whoisdata,settingsvars=settingsvars)
+        if settingsvars['whoisinfo'] == "on":
+            if str(http['inputtype']) == "Domain":
+                address= str(whoisdata['city']) + ", " + str(whoisdata['country'])
+            else:
+                address = str(whoisdata['nets'][0]['city']) + ", " + str(whoisdata['nets'][0]['country'])
+        else:
+            address="Information about " + str(http['object'])
+        return render_template('networkobject.html', records=http, jsonvt=jsonvt, whoisdata=whoisdata,settingsvars=settingsvars, address=address)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+@app.route('/threatactors/<uid>/info', methods=['GET'])
+def threatactorobject(uid):
+    try:
+        http = mongo.db.network.find_one({'_id':bson.ObjectId(oid=str(uid))})
+        return render_template('threatactorobject.html', records=http)
     except Exception as e:
         return render_template('error.html', error=e)
 
@@ -334,7 +357,7 @@ def unfavorite(uid):
 @app.route('/settings', methods=['GET'])
 def settings():
     try:
-        records = mongo.db.settings.find()
+        records = mongo.db.settings.find_one()
         return render_template('settings.html', records=records)
     except Exception as e:
         return render_template('error.html', error=e)
@@ -349,12 +372,12 @@ def delete():
     except Exception as e:
         return render_template('error.html', error=e)
 
-@app.route('/test', methods=['GET'])
-def test():
-    try:
-        return render_template('test.html')
-    except Exception as e:
-        return render_template('error.html', error=e)
+@app.route('/download/<uid>')
+def download(uid):
+    http = mongo.db.network.find_one({'_id':bson.ObjectId(oid=str(uid))})
+    response = make_response(str(convert(http)))
+    response.headers["Content-Disposition"] = "attachment; filename="+uid+".txt"
+    return response
 
 @app.errorhandler(404)
 def not_found(error):
@@ -382,7 +405,7 @@ def totalcount():
 # Total Network Indicators
 @app.context_processor
 def networkcount():
-    return dict(networkcount=mongo.db.network.find({"$or": [{"inputtype": "IPv4"}, {"inputtype": "IPv6"}, {"inputtype": "Domain"}]}).count())
+    return dict(networkcount=mongo.db.network.find({"$or": [{"inputtype": "IPv4"}, {"inputtype": "IPv6"}, {"inputtype":"Network"}, {"inputtype": "Domain"}]}).count())
 
 # Total Threat Actor Indicators
 @app.context_processor
