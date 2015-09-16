8d56abb621ad8de4586b390090ad2b764aa49fca
root <root@fawkes.hrck.lan>
2015-09-15 21:15:56 -0500

    moving newobject submit and cancel buttons to top

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ import csv
 # Configuration #
 #################
 app = Flask(__name__)
-app.config['MONGO_HOST'] = '127.0.0.1'
+app.config['MONGO_HOST'] = 'localhost'
 app.config['MONGO_PORT'] = 27017
 app.config['MONGO_DBNAME'] = 'threatnote'
 
