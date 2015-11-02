4a36db7e1f5c1f302c19864b42e2789e151e99d6
alxhrck <alex@hrck.net>
2015-11-02 15:52:04 -0600

    Updated proxy code

--- libs/helpers.py
+++ libs/helpers.py
@@ -1,10 +1,18 @@
 import collections
+import sqlite3 as lite
 
 def get_proxy():
     try:
+        con = lite.connect('threatnote.db')
+        con.row_factory = lite.Row
+        with con:
+            cur = con.cursor()
+            cur.execute("SELECT httpproxy,httpsproxy from settings")
+            httpproxy = cur.fetchall()[0][0]
+            #httpsproxy = cur.fetchall()[1][0]
         proxies = {
-            #'http': mongo.db.settings.find_one()['httpproxy'],
-            #'https': mongo.db.settings.find_one()['httpsproxy'],
+            'http': httpproxy,
+            'https': httpproxy,
         }
     except KeyError:
         proxies = {}
