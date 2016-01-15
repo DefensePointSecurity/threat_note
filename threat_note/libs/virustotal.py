a41d95c146c33b05221dfaea7f9c2658f90ba42a
Scott J Roberts <sroberts@github.com>
2016-01-15 10:58:03 -0500

    fixed libs reference

--- threat_note/libs/virustotal.py
+++ threat_note/libs/virustotal.py
@@ -11,7 +11,7 @@ def vt_ipv4_lookup(ipv4):
         apikey = settings.apikey
         url = "https://www.virustotal.com/vtapi/v2/ip-address/report"
         params = {'ip': ipv4, 'apikey': apikey}
-        r = requests.get(url, params=params, verify=False, proxies=libs.helpers.get_proxy())
+        r = requests.get(url, params=params, verify=False, proxies=helpers.get_proxy())
         j = json.loads(r.text)
         j['resolutions'] = sorted(j['resolutions'], key=lambda k: k['last_resolved'], reverse=True)
         return j
@@ -26,7 +26,7 @@ def vt_domain_lookup(domain):
         apikey = settings.apikey
         url = "https://www.virustotal.com/vtapi/v2/domain/report"
         params = {'domain': domain, 'apikey': apikey}
-        r = requests.get(url, params=params, verify=False, proxies=libs.helpers.get_proxy())
+        r = requests.get(url, params=params, verify=False, proxies=helpers.get_proxy())
         j = json.loads(r.text)
         j['resolutions'] = sorted(j['resolutions'], key=lambda k: k['last_resolved'], reverse=True)
         if len(j) < 20:
@@ -43,7 +43,7 @@ def vt_hash_lookup(filehash):
         apikey = settings.apikey
         url = "https://www.virustotal.com/vtapi/v2/file/report"
         params = {'resource': filehash, 'apikey': apikey}
-        r = requests.get(url, params=params, verify=False, proxies=libs.helpers.get_proxy())
+        r = requests.get(url, params=params, verify=False, proxies=helpers.get_proxy())
         j = json.loads(r.text)
         return j
     except:
